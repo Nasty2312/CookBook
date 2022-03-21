c0e80ab8576882e1d585f5cf201db1cf23cf7a6f
Nasty2312 <85288357+Nasty2312@users.noreply.github.com>
2022-03-22 00:05:56 +0300

    Add files via upload

---
+++ CookBook_Modestova_Dorina/CookBook_Modestova_Dorina WatchKit Extension/RowTable.swift
@@ -0,0 +1,14 @@
+//
+//  RowTable.swift
+//  CookBook_Modestova_Dorina WatchKit Extension
+//
+//  Created by Student on 28.02.2022.
+//
+
+import WatchKit
+
+class RowTable: NSObject {
+    @IBOutlet weak var rowRicture: WKInterfaceImage!
+    @IBOutlet weak var rowRecipeName: WKInterfaceLabel!
+
+}
